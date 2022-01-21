9761d23642a3f45ac33e61d36bd506061392528d
Caio Louro <caio.louro@vtex.com.br>
2022-01-21 15:20:02 -0300

    rotate array optimized version

--- leetcode/rotate-array.js
+++ leetcode/rotate-array.js
@@ -3,7 +3,7 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
+var rotateV1 = function (nums, k) { // Timedout for long inputs
     for (let i = 0; i < k; i++) {
         let replaced
         let nextValue
@@ -28,4 +28,25 @@ var rotate = function (nums, k) {
     console.log('nums', JSON.stringify(nums, null, 4))
 }
 
-rotate([2147483647,-2147483648,33,219,0], 4)
+var rotate = function (nums, k) {
+    var reverse = function (nums, start, end) {
+        while (start < end) {
+            const temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start++
+            end--
+        }
+    }
+
+    let newK = k > nums.length ? k % nums.length : k
+
+    reverse(nums, 0, nums.length - newK - 1)
+    reverse(nums, nums.length - newK, nums.length - 1)
+    reverse(nums, 0, nums.length - 1)
+
+    console.log('nums', JSON.stringify(nums, null, 4))
+}
+
+rotate([2147483647,-2147483648,33,219,0], 6)
+// rotate([-1,-100,3,99], 2)
